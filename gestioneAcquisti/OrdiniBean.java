7142972572afbf4193b1f1ed280b59111deb213f
VinTar01 <V.Tarantino96@gmail.com>
2023-11-03 16:09:40 +0100

    Create OrdiniBean.java

---
+++ gestioneAcquisti/OrdiniBean.java
@@ -0,0 +1,29 @@
+package gestioneAcquisti;
+
+public class OrdiniBean
+{
+    //rappresenta il carello?
+    private String idemail;
+    private String codiceProdotto;
+    
+    @Override
+    public String toString() {
+        return "SessionCarrelloBean [idemail=" + this.idemail + ", codiceProdotto=" + this.codiceProdotto + ", getIdemail()=" + this.getIdemail() + ", getCodiceProdotto()=" + this.getCodiceProdotto() + ", getClass()=" + this.getClass() + ", hashCode()=" + this.hashCode() + ", toString()=" + super.toString() + "]";
+    }
+    
+    public String getIdemail() {
+        return this.idemail;
+    }
+    
+    public void setIdemail(final String idemail) {
+        this.idemail = idemail;
+    }
+    
+    public String getCodiceProdotto() {
+        return this.codiceProdotto;
+    }
+    
+    public void setCodiceProdotto(final String codiceProdotto) {
+        this.codiceProdotto = codiceProdotto;
+    }
+}
