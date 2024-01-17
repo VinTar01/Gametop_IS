2239d4fd649c80c7adca4c451fa02e32e583f3a0
VinTar01 <133966826+VinTar01@users.noreply.github.com>
2024-01-17 21:29:54 +0100

    Final version GestioneRuoliDAO

--- src/test/java/TestGestioneRuoliDAO.java
+++ src/test/java/TestGestioneRuoliDAO.java
@@ -1,127 +1,365 @@
-   package test.java;
+import static org.junit.jupiter.api.Assertions.*;
 
-import gestioneAccount.PersonaleBean;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Collection;
-import java.util.LinkedList;
+
 import javax.sql.DataSource;
+
 import org.dbunit.IDatabaseTester;
 import org.dbunit.JdbcDatabaseTester;
-import org.dbunit.operation.DatabaseOperation;
-import org.h2.Driver;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import test.java.GestoreRuoliModelDS;
 
-public class TestGestioneRuoliDAO {
-   private Connection db;
-   private GestoreRuoliModelDS personaleDAO;
-   private PersonaleBean personaleEsistente;
-   private PersonaleBean personaleNonEsistente;
-   private static IDatabaseTester tester;
-   private DataSource ds;
+//import gestioneAccount.GestorePersonaleModelDS;
 
-   @Before
-   public void setUp() throws SQLException, Exception {
-      tester = new JdbcDatabaseTester(Driver.class.getName(), "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;init=runscript from 'resources/db/init/schema.sql'", "PROVA", "");
-      tester.setSetUpOperation(DatabaseOperation.REFRESH);
-      tester.setTearDownOperation(DatabaseOperation.DELETE_ALL);
-      this.ds = (DataSource)Mockito.mock(DataSource.class);
-      Mockito.when(this.ds.getConnection()).thenReturn(tester.getConnection().getConnection());
-      this.personaleDAO = new GestoreRuoliModelDS(this.ds);
-      this.personaleEsistente = new PersonaleBean();
-      this.personaleEsistente.setNome("Francesco1");
-      this.personaleEsistente.setCognome("Leo1");
+import gestioneAccount.LoginModelDS;
+import gestioneAccount.PersonaleBean;
+import gestioneAccount.UtenteBean;
+import testingM.GestorePersonaleModelDS2;
+import testingM.RegistrazioneModelDS2;
+
+class TestGestioneRuoliDAO {
+
+	private static final String JDBC_URL = "jdbc:h2:mem:test;MODE=MySQL;DB_CLOSE_DELAY=-1";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "";
+    
+    private DataSource dataSource;
+    
+    private PersonaleBean personaleEsistente, personaleNonEsistente;
+
+    private RegistrazioneModelDS2 UtenteDAO;
+    private GestorePersonaleModelDS2 PersonaleDAO;
+    private LoginModelDS LoginDAO;
+    
+    private Connection dbConnection;
+    
+    private DataSource ds;
+    private static IDatabaseTester tester;
+    
+ 
+    
+
+    
+    
+    
+    @Before
+    public void setUp() throws Exception {
+    	dbConnection=null;
+    	 
+    	    
+    	  try {
+    		    Class.forName("com.mysql.jdbc.Driver");
+    		    dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/GametopDB", "root", "31032020VinTar01");
+    		    System.out.println("Connessione riuscita!");
+    		} catch (Exception e) {
+    		    System.out.println("Connessione fallita!");
+    		    e.printStackTrace();
+    		} finally {
+    		    if (dbConnection != null) {
+    		        try {
+    		            dbConnection.close();
+    		            System.out.println("Connessione chiusa correttamente!");
+    		        } catch (SQLException sqe) {
+    		            System.out.println("Impossibile chiudere la connessione in modo pulito.");
+    		            sqe.printStackTrace();
+    		        }
+    		    }
+    		}
+         
+       
+        
+    }
+    
+    
+    
+    @Test
+    public void TestFindPersonaleByEmailExisting() throws Exception {
+    	Class.forName("com.mysql.jdbc.Driver");
+	    dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/GametopDB", "root", "31032020VinTar01");
+	   
+	    tester = new JdbcDatabaseTester(org.h2.Driver.class.getName(),
+	    	    "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;init=runscript from 'resources/db/init/schema.sql'",
+	    	    "PROVA",
+	    	    ""
+	    	    );
+	    ds = Mockito.mock(DataSource.class);
+	    Mockito.when(ds.getConnection()).thenReturn(tester.getConnection().getConnection());
+	    
+	    personaleEsistente = new PersonaleBean();
+	    PersonaleDAO = new GestorePersonaleModelDS2(ds);
+        
+  
+       
+      
+      this.personaleEsistente.setNome("francesco");
+      this.personaleEsistente.setCognome("leo");
       this.personaleEsistente.setEmail("francesco1@gmail.com");
-      this.personaleEsistente.setPassword("ABCD1234");
+      this.personaleEsistente.setPassword("ABCD123456");
       this.personaleEsistente.setRuolo("Gestore Ordini");
-      this.personaleDAO.inserisciPersonale(this.personaleEsistente);
-      this.personaleNonEsistente = new PersonaleBean();
-      this.personaleNonEsistente.setNome("Francesco2");
-      this.personaleNonEsistente.setCognome("Leo2");
+      this.PersonaleDAO.inserisciPersonale(this.personaleEsistente);
+         
+		assertEquals(personaleEsistente, PersonaleDAO.doRetrieveByKey(personaleEsistente.getEmail()));
+	}
+    
+    
+    
+    @Test
+    public void TestFindPersonaleByEmailNull() throws Exception {
+    	Class.forName("com.mysql.jdbc.Driver");
+	    dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/GametopDB", "root", "31032020VinTar01");
+	   
+	    tester = new JdbcDatabaseTester(org.h2.Driver.class.getName(),
+	    	    "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;init=runscript from 'resources/db/init/schema.sql'",
+	    	    "PROVA",
+	    	    ""
+	    	    );
+	    ds = Mockito.mock(DataSource.class);
+	    Mockito.when(ds.getConnection()).thenReturn(tester.getConnection().getConnection());
+	    
+	    PersonaleDAO = new GestorePersonaleModelDS2(ds);
+       
+      
+       assertNotEquals(this.personaleEsistente, this.PersonaleDAO.doRetrieveByKey((String)null));
+    }
+       
+    
+    @Test
+    public void TestFindPersonaleByEmailNotExisting() throws Exception {
+    	Class.forName("com.mysql.jdbc.Driver");
+	    dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/GametopDB", "root", "31032020VinTar01");
+	   
+	    tester = new JdbcDatabaseTester(org.h2.Driver.class.getName(),
+	    	    "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;init=runscript from 'resources/db/init/schema.sql'",
+	    	    "PROVA",
+	    	    ""
+	    	    );
+	    ds = Mockito.mock(DataSource.class);
+	    Mockito.when(ds.getConnection()).thenReturn(tester.getConnection().getConnection());
+	   
+      
+	    
+	    personaleNonEsistente = new PersonaleBean();
+	    PersonaleDAO = new GestorePersonaleModelDS2(ds);
+       
+      
+      this.personaleNonEsistente.setNome("francesco");
+      this.personaleNonEsistente.setCognome("leo");
       this.personaleNonEsistente.setEmail("francesco2@gmail.com");
-      this.personaleNonEsistente.setPassword("ABCD1234");
+      this.personaleNonEsistente.setPassword("ABCD123456");
       this.personaleNonEsistente.setRuolo("Gestore Ordini");
-   }
+      
+         
+		assertNotEquals(personaleNonEsistente, PersonaleDAO.doRetrieveByKey(personaleNonEsistente.getEmail()));
+	}
+    
+    
+    @Test
+    public void TestDeletePersonale() throws Exception {
+    	Class.forName("com.mysql.jdbc.Driver");
+	    dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/GametopDB", "root", "31032020VinTar01");
+	   
+	    tester = new JdbcDatabaseTester(org.h2.Driver.class.getName(),
+	    	    "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;init=runscript from 'resources/db/init/schema.sql'",
+	    	    "PROVA",
+	    	    ""
+	    	    );
+	    ds = Mockito.mock(DataSource.class);
+	    Mockito.when(ds.getConnection()).thenReturn(tester.getConnection().getConnection());
+	    
+	    personaleEsistente = new PersonaleBean();
+	    PersonaleDAO = new GestorePersonaleModelDS2(ds);
+        
+      
+      this.personaleEsistente.setNome("francesco");
+      this.personaleEsistente.setCognome("leo");
+      this.personaleEsistente.setEmail("francesco3@gmail.com");
+      this.personaleEsistente.setPassword("ABCD123456");
+      this.personaleEsistente.setRuolo("Gestore Ordini");
+      this.PersonaleDAO.inserisciPersonale(this.personaleEsistente);
+      this.PersonaleDAO.doDelete(this.personaleEsistente);
+      assertNotEquals(this.personaleEsistente, this.PersonaleDAO.doRetrieveByKey("francesco3@gmail.com")); 
+    }
 
-   @Test
-   public void TestFindPersonaleByEmailExisting() throws SQLException {
-      Assertions.assertEquals(this.personaleEsistente, this.personaleDAO.doRetrieveByKey("francesco1@gmail.com"));
-   }
+    @Test
+    public void TestDeletePersonaleNonEsistente() throws Exception {
+    	Class.forName("com.mysql.jdbc.Driver");
+	    dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/GametopDB", "root", "31032020VinTar01");
+	   
+	    tester = new JdbcDatabaseTester(org.h2.Driver.class.getName(),
+	    	    "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;init=runscript from 'resources/db/init/schema.sql'",
+	    	    "PROVA",
+	    	    ""
+	    	    );
+	    ds = Mockito.mock(DataSource.class);
+	    Mockito.when(ds.getConnection()).thenReturn(tester.getConnection().getConnection());
+	   
+      
+	    
+	    personaleNonEsistente = new PersonaleBean();
+	    PersonaleDAO = new GestorePersonaleModelDS2(ds);
+       
+      
+      this.personaleNonEsistente.setNome("francesco");
+      this.personaleNonEsistente.setCognome("leo");
+      this.personaleNonEsistente.setEmail("francesco6@gmail.com");
+      this.personaleNonEsistente.setPassword("ABCD123456");
+      this.personaleNonEsistente.setRuolo("Gestore Ordini");
+      
+      assertEquals(false, this.PersonaleDAO.doDelete(this.personaleNonEsistente));
+    }
 
-   @Test
-   public void TestFindPersonaleByEmailNull() throws SQLException {
-      Assert.assertNotEquals(this.personaleEsistente, this.personaleDAO.doRetrieveByKey((String)null));
-   }
+    @Test
+    public void TestDeletePersonaleNull() throws Exception {
+    	Class.forName("com.mysql.jdbc.Driver");
+	    dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/GametopDB", "root", "31032020VinTar01");
+	   
+	    tester = new JdbcDatabaseTester(org.h2.Driver.class.getName(),
+	    	    "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;init=runscript from 'resources/db/init/schema.sql'",
+	    	    "PROVA",
+	    	    ""
+	    	    );
+	    ds = Mockito.mock(DataSource.class);
+	    Mockito.when(ds.getConnection()).thenReturn(tester.getConnection().getConnection());
+	    
+	    
+	    PersonaleDAO = new GestorePersonaleModelDS2(ds);
+       assertThrows(NullPointerException.class, () -> {
+          this.PersonaleDAO.doDelete((PersonaleBean)null);
+       });
+    }
 
-   @Test
-   public void TestFindPersonaleByEmailNotExisting() throws SQLException {
-      Assert.assertNotEquals(this.personaleNonEsistente, this.personaleDAO.doRetrieveByKey("francesco2@gmail.com"));
-   }
+    @Test
+    public void TestFindTuttoPersonale() throws Exception {
+    	Class.forName("com.mysql.jdbc.Driver");
+	    dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/GametopDB", "root", "31032020VinTar01");
+	   
+	    tester = new JdbcDatabaseTester(org.h2.Driver.class.getName(),
+	    	    "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;init=runscript from 'resources/db/init/schema.sql'",
+	    	    "PROVA",
+	    	    ""
+	    	    );
+	    ds = Mockito.mock(DataSource.class);
+	    Mockito.when(ds.getConnection()).thenReturn(tester.getConnection().getConnection());
+	    
+	    
+	    PersonaleDAO = new GestorePersonaleModelDS2(ds);
+    	
+    	
+    	
+       Collection<PersonaleBean> expected = this.PersonaleDAO.stampaTuttoIlPersonale();
+      
+       assertEquals(expected, this.PersonaleDAO.stampaTuttoIlPersonale());
+    }
 
-   @Test
-   public void TestDeletePersonale() throws SQLException {
-      this.personaleDAO.doDelete(this.personaleEsistente);
-      Assert.assertNotEquals(this.personaleEsistente, this.personaleDAO.doRetrieveByKey("francesco2@gmail.com"));
-   }
+    @Test
+    public void TestUpdatePersonale() throws Exception {
+    	Class.forName("com.mysql.jdbc.Driver");
+	    dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/GametopDB", "root", "31032020VinTar01");
+	   
+	    tester = new JdbcDatabaseTester(org.h2.Driver.class.getName(),
+	    	    "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;init=runscript from 'resources/db/init/schema.sql'",
+	    	    "PROVA",
+	    	    ""
+	    	    );
+	    ds = Mockito.mock(DataSource.class);
+	    Mockito.when(ds.getConnection()).thenReturn(tester.getConnection().getConnection());
+	    
+	    personaleEsistente = new PersonaleBean();
+	    PersonaleDAO = new GestorePersonaleModelDS2(ds);
+	    
+       this.personaleEsistente.setNome("Francesco3");
+       this.personaleEsistente.setCognome("Leo3");
+       this.personaleEsistente.setPassword("ABCD1234");
+       this.personaleEsistente.setEmail("kekkoleo12@gmail.com");
+       this.personaleEsistente.setRuolo("Gestore prodotti");
+       this.PersonaleDAO.inserisciPersonale(this.personaleEsistente);
+       
+       this.personaleEsistente.setNome("Francesco4");
+       this.personaleEsistente.setCognome("Leo4");
+       this.personaleEsistente.setPassword("ABCD12345");
+       this.personaleEsistente.setEmail("kekkoleo13@gmail.com");
+       this.personaleEsistente.setRuolo("Gestore prodotti");
+       this.PersonaleDAO.doUpdate(this.personaleEsistente);
+       
+       this.personaleEsistente = this.PersonaleDAO.doRetrieveByKey("kekkoleo13@gmail.com");
+       assertEquals(this.personaleEsistente.getNome(), this.PersonaleDAO.doRetrieveByKey("kekkoleo13@gmail.com").getNome());
+       assertEquals(this.personaleEsistente.getCognome(), this.PersonaleDAO.doRetrieveByKey("kekkoleo13@gmail.com").getCognome());
+       assertEquals(this.personaleEsistente.getEmail(), this.PersonaleDAO.doRetrieveByKey("kekkoleo13@gmail.com").getEmail());
+       assertEquals(this.personaleEsistente.getPassword(), this.PersonaleDAO.doRetrieveByKey("kekkoleo13@gmail.com").getPassword());
+       assertEquals(this.personaleEsistente.getRuolo(), this.PersonaleDAO.doRetrieveByKey("kekkoleo13@gmail.com").getRuolo());
+    }
 
-   @Test
-   public void TestDeletePersonaleNonEsistente() throws SQLException {
-      Assertions.assertEquals(false, this.personaleDAO.doDelete(this.personaleNonEsistente));
-   }
+    @Test
+    public void TestUpdatePersonaleNonEsistente() throws Exception {
+    	Class.forName("com.mysql.jdbc.Driver");
+	    dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/GametopDB", "root", "31032020VinTar01");
+	   
+	    tester = new JdbcDatabaseTester(org.h2.Driver.class.getName(),
+	    	    "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;init=runscript from 'resources/db/init/schema.sql'",
+	    	    "PROVA",
+	    	    ""
+	    	    );
+	    ds = Mockito.mock(DataSource.class);
+	    Mockito.when(ds.getConnection()).thenReturn(tester.getConnection().getConnection());
+	    
+	    personaleNonEsistente = new PersonaleBean();
+	    PersonaleDAO = new GestorePersonaleModelDS2(ds);
+	    
+       
+      
+       this.personaleNonEsistente.setNome("Francesco4");
+       this.personaleNonEsistente.setCognome("Leo4");
+       this.personaleNonEsistente.setPassword("ABCD12345");
+       this.personaleNonEsistente.setEmail("kekkoleononesiste13@gmail.com");
+       this.personaleNonEsistente.setRuolo("Gestore prodotti");
+       
+       
+       personaleNonEsistente = PersonaleDAO.doRetrieveByKey(personaleNonEsistente.getEmail());
+       personaleNonEsistente.setNome("Francesco5");
+       personaleNonEsistente.setCognome("Leo5");
+       personaleNonEsistente.setEmail("kekkoleononesistente14@gmail.com");
+       personaleNonEsistente.setRuolo("Gestore prodotti");
+       personaleNonEsistente.setPassword("ABCD1234");
 
-   @Test
-   public void TestDeletePersonaleNull() throws SQLException {
-      Assertions.assertThrows(NullPointerException.class, () -> {
-         this.personaleDAO.doDelete((PersonaleBean)null);
-      });
-   }
+       assertNotEquals(false, PersonaleDAO.doUpdate(personaleNonEsistente));
+      }
+    
+    
+  
+    
+    @Test
+    public void TestUpdatePersonaleNull() throws Exception {
+    	Class.forName("com.mysql.jdbc.Driver");
+	    dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/GametopDB", "root", "31032020VinTar01");
+	   
+	    tester = new JdbcDatabaseTester(org.h2.Driver.class.getName(),
+	    	    "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;init=runscript from 'resources/db/init/schema.sql'",
+	    	    "PROVA",
+	    	    ""
+	    	    );
+	    ds = Mockito.mock(DataSource.class);
+	    Mockito.when(ds.getConnection()).thenReturn(tester.getConnection().getConnection());
+	    
+	    PersonaleDAO = new GestorePersonaleModelDS2(ds);
+    	assertThrows(NullPointerException.class, () ->{
+    		PersonaleDAO.doUpdate(null);
+        });
+    }
+    
+    
 
-   @Test
-   public void TestFindTuttoPersonale() throws SQLException {
-      Collection<PersonaleBean> expected = new LinkedList();
-      expected.add(this.personaleEsistente);
-      Assertions.assertEquals(expected, this.personaleDAO.stampaTuttoIlPersonale());
-   }
+	@After
+    public void tearDown() throws SQLException {
+        // Pulisci il database dopo l'esecuzione dei test
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD)) {
+            connection.createStatement().execute("DROP ALL OBJECTS DELETE FILES");
+        }
+	}
+	
 
-   @Test
-   public void TestUpdatePersonale() throws SQLException {
-      this.personaleEsistente.setNome("Francesco3");
-      this.personaleEsistente.setCognome("Leo3");
-      this.personaleEsistente.setPassword("ABCD1234");
-      this.personaleEsistente.setRuolo("Gestore prodotti");
-      this.personaleDAO.doUpdate(this.personaleEsistente);
-      this.personaleEsistente = this.personaleDAO.doRetrieveByKey("francesco1@gmail.com");
-      Assertions.assertEquals(this.personaleEsistente.getNome(), this.personaleDAO.doRetrieveByKey("francesco1@gmail.com").getNome());
-      Assertions.assertEquals(this.personaleEsistente.getCognome(), this.personaleDAO.doRetrieveByKey("francesco1@gmail.com").getCognome());
-      Assertions.assertEquals(this.personaleEsistente.getEmail(), this.personaleDAO.doRetrieveByKey("francesco1@gmail.com").getEmail());
-      Assertions.assertEquals(this.personaleEsistente.getPassword(), this.personaleDAO.doRetrieveByKey("francesco1@gmail.com").getPassword());
-      Assertions.assertEquals(this.personaleEsistente.getRuolo(), this.personaleDAO.doRetrieveByKey("francesco1@gmail.com").getRuolo());
-   }
-
-   @Test
-   public void TestUpdatePersonaleNonEsistente() throws SQLException {
-      this.personaleEsistente.setNome("Francesco3");
-      this.personaleEsistente.setCognome("Leo3");
-      this.personaleEsistente.setPassword("ABCD1234");
-      this.personaleEsistente.setRuolo("Gestore prodotti");
-      Assertions.assertEquals(false, this.personaleDAO.doUpdate(this.personaleNonEsistente));
-   }
-
-   @Test
-   public void TestUpdatePersonaleNull() throws SQLException {
-      Assertions.assertEquals(false, this.personaleDAO.doUpdate((PersonaleBean)null));
-   }
-
-   @After
-   public void tearDown() throws SQLException {
-      System.out.println("Sono entrato nella tearDown");
-      this.personaleDAO.doDelete(this.personaleEsistente);
-      this.personaleDAO.doDelete(this.personaleNonEsistente);
-   }
 }
